91d2eb22df843444e7bfe4865a9f8afb9217a811
mahdihawary <Mahdihawary@gmail.com>
2023-03-21 16:26:09 -0400

    updated home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,13 @@
+import { Typography } from "@mui/material";
+
 const HomePage = function () {
   return (
     <>
       <h1>Mahdi Hawary</h1>
+      <Button>Projects</Button>
+      <container>
+        <Typography>Short description</Typography>
+      </container>
     </>
   );
 };
