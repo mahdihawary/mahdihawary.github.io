733ee424d0da5e00b9ee4a75a3dba2e3e41733f6
mahdihawary <Mahdihawary@gmail.com>
2023-03-22 16:33:40 -0400

    added public url compat

--- src/App.js
+++ src/App.js
@@ -35,10 +35,22 @@ function App() {
           duration={3000}
         >
           <Routes>
-            <Route path="/" element={<HomePage />}></Route>
-            <Route path="/about" element={<AboutPage />}></Route>
-            <Route path="/projects" element={<ProjectsPage />}></Route>
-            <Route path="/Contact" element={<ContactPage />}></Route>
+            <Route
+              path={process.env.PUBLIC_URL + "/"}
+              element={<HomePage />}
+            ></Route>
+            <Route
+              path={process.env.PUBLIC_URL + "/About"}
+              element={<AboutPage />}
+            ></Route>
+            <Route
+              path={process.env.PUBLIC_URL + "/Projects"}
+              element={<ProjectsPage />}
+            ></Route>
+            <Route
+              path={process.env.PUBLIC_URL + "/Contact"}
+              element={<ContactPage />}
+            ></Route>
             <Route path="*" element={<NotFound />}></Route>
           </Routes>
         </Container>
